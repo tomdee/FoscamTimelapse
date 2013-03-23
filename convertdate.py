6f6aad55355f14220778377efe42b22dbc03cb6c
Tom Denham <tom@tomdee.co.uk>
2013-03-23 11:56:54 +0000

    Allow choosing what date create timelapse for - defaults to today

--- convertdate.py
+++ convertdate.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 import sys, datetime, glob, os;
-# Pass in a glob argument.
-# This script will then extract the date from the default filename format used by FOSCAMs
-# And then use imagemagick to put a date/time on the corner of each image, storing the results in /tmp/timelapse
 
-from2 = "%Y%m%d%H%M%S"
-to2 = "%A, %d %B %Y %H:%M %Ss"
+# Pass in a glob argument as the first arg
+# and a directory as a second arg.
+
+# This script will then extract the date from the default filename format used by FOSCAMs
+# And then use imagemagick to put a date/time on the corner of each image
+
+fromFormat = "%Y%m%d%H%M%S"
+toFormat   = "%A, %d %B %Y %H:%M %Ss"
 
 for name in glob.glob(sys.argv[1]):
 	splitName = name.split("_")
 	if (len(splitName) == 4):
-		dateString = datetime.datetime.strptime(splitName[2], from2).strftime(to2)
-		os.system('convert \'%s\' -gravity SouthEast -pointsize 24 -undercolor "#00000080" -fill white -annotate +12+12 "%s"  \'/tmp/timelapse/%s.converted.jpg\'' % (name,dateString,name))
+		dateString = datetime.datetime.strptime(splitName[2], fromFormat).strftime(toFormat)
+		os.system('convert \'%s\' -gravity SouthEast -pointsize 24 -undercolor "#00000080" -fill white -annotate +12+12 "%s"  \'%s/%s.converted.jpg\'' % (name,dateString,sys.argv[2],name))
--- createdailytimelapse.sh
+++ createdailytimelapse.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-mkdir /tmp/timelapse
-/root/convertdate.py  "*_$(date +%Y%m%d)*.jpg"
-cd /tmp/timelapse
-mencoder "mf://*.jpg" -mf fps=30 -o /storage/Bryantwood$(date +%Y%m%d).avi -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=3600
-rm -rf /tmp/timelapse
+#Default to today's date, but allow a different date (in YYYYMMDD format) to be passed in.
+
+if [[ $1 ]]; then
+  DATE=$1
+else
+  DATE=`date +%Y%m%d`
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMP_DIR="/tmp/timelapse"
+OUTPUT_DIR="/storage"
+
+echo $SCRIPT_DIR
+echo $DATE
+mkdir $TEMP_DIR
+$SCRIPT_DIR/convertdate.py  "*_$DATE*.jpg" $TEMP_DIR
+cd $TEMP_DIR
+mencoder "mf://*.jpg" -mf fps=30 -o $OUTPUT_DIR/Bryantwood$DATE.avi -ovc x264 -x264encopts preset=medium:global_header:threads=auto
+rm -rf $TEMP_DIR
